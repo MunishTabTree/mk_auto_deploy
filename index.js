44bedd668898218e14f0000fefcf8756fbf42dc8
MK <munish.tabtree@gmail.com>
2022-06-27 12:35:04 +0530

    new route added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) =>{
-    res.send("hello munish & pbk & sabari")
+    res.send("hello munish & pbk & sabari & madhes")
 })
 
 app.get('/:name', (req, res) =>{
